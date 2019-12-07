782568e39e635f7dc83c95d2a29c6a859376740e
Alan Suarez <Alan.Andre.Suarez@gmail.com>
2019-12-07 15:58:27 -0800

    Created Top Spots Project

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
-// write your code here
-
+	$.getJSON('data.json', (topSpotsArray) => {
+  	$.each(topSpotsArray, function(property, val) {
+			$('#top-spots').append(
+				'<tr>' + 
+					'<td class="name" style="background-color: lightseagreen">' + val.name + '</td>' +
+					'<td>' + val.description + '</td>' + 
+					'<td style="background-color: lightseagreen">' + 
+						'<input type="button" onclick="location.href=" style="background-color: lightseagreen; border: none; color: white; cursor: pointer;" '+ 
+						"'https://www.google.com/maps?q=" + val.location + "'" + ';" value="Open in Google Maps"' + '/>' + 
+					'</td>' + 
+				'</tr>'); 
+			console.log(val.location)
+		});
+	});
 });
