bc94aacf9c570a011cb0e5d2947fe088830b0fa8
Alan Suarez <Alan.Andre.Suarez@gmail.com>
2019-12-08 11:20:55 -0800

    Fixed Top Spots Project

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
 	$.getJSON('data.json', (topSpotsArray) => {
   	$.each(topSpotsArray, function(property, val) {
-			$('#top-spots').append(
-				'<tr>' + 
-					'<td class="name" style="background-color: lightseagreen">' + val.name + '</td>' +
-					'<td>' + val.description + '</td>' + 
-					'<td style="background-color: lightseagreen">' + 
-						'<input type="button" onclick="location.href=" style="background-color: lightseagreen; border: none; color: white; cursor: pointer;" '+ 
-						"'https://www.google.com/maps?q=" + val.location + "'" + ';" value="Open in Google Maps"' + '/>' + 
-					'</td>' + 
-				'</tr>'); 
-			console.log(val.location)
+			$('#top-spots').append(`
+			<tr>
+				<td style='background-color: lightseagreen'>${val.name}</td>
+				<td>${val.description}</td>
+				<td style='background-color: lightseagreen; border: none; color: white; cursor: pointer'>
+					<a href="https://www.google.com/maps?q=${val.location}">Open in Google Maps</a>
+				</td>`)
 		});
 	});
 });
